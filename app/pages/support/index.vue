5ed9e066e640089f098b39b45ecece2784be0622
Aurelien Roux <aureroux13@gmail.com>
2026-01-27 17:51:39 +0100

    i18n functional

---
+++ app/pages/support/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <StoryblokComponent v-if="story" :blok="story.content" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const { locale } = useI18n()
+
+// French (default): "support" | English: "en/support"
+const slug = locale.value === 'en' ? 'en/support' : 'support'
+
+const { story } = await useAsyncStoryblok(slug, {
+  api: {
+    version: "draft",
+  },
+  bridge: {},
+});
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: [
-    "@nuxtjs/i18n",
     "@nuxtjs/google-fonts",
     [
       "@storyblok/nuxt",
@@ -11,6 +10,7 @@ export default defineNuxtConfig({
         accessToken: process.env.STORYBLOK_DELIVERY_API_TOKEN,
       },
     ],
+    "@nuxtjs/i18n",
   ],
   css: ["~/assets/scss/index.scss"],
   vite: {
@@ -34,5 +34,14 @@ export default defineNuxtConfig({
   },
   storyblok: {
     accessToken: process.env.STORYBLOK_ACCESS_TOKEN,
+  },
+  i18n: {
+    locales: [
+      { code: "fr", iso: "fr-FR", file: "fr.json" },
+      { code: "en", iso: "en-US", file: "en.json" },
+    ],
+    defaultLocale: "fr",
+    strategy: "prefix_except_default",
+    langDir: "locales",
   },
 });
